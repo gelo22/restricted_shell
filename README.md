cf511efbdec7f4d4b44ad6827b8b6083264c2ae9
alex <alexey.kurinnij@gmail.com>
2017-05-28 13:08:03 +0300

    config and log files per user, debug mode, functions

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# restricted_shell
+### Purpose
+
+allow only listed commands for particular ssh connection identified by public key
+
+### Example usage
+
+1. Clone git
+2. Create new ssh key for this script, authorize this ssh key on the destination host
+3. Make your own config from example ".py.conf"
+4. Run desired command via ssh
+5. Add command from log to config
+
+my_server_host - client(s) will connect to this host
+my_client_host - client host, which will connect to my_server_host
+my_ssh_key - public ssh key which my_client_host will use for connection to my_server_host
+my_user - user, which my_client_host will use for connection to my_server_host
+
+# on the my_client_host todo:
+# login as desired user, generate ssh_keys (by default id_rsa and id_rsa.pub)
+ssh-keygen
+
+# on the my_server_host todo:
+
+### Ruquirements
+
+Python2 or Python3
+
+GNU/Linux
+
+Openssh
+
+### Options
+
+Config file and script have the same options, run script in help mode to get list of options:
+./.py -h
+
+Options from command line will owerride values from config
+
--- restricted_shell.py
+++ restricted_shell.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from __future__ import print_function
 from __future__ import division
@@ -14,45 +14,77 @@ import argparse
 import sys
 import traceback
 
-# parse args
-parser = argparse.ArgumentParser()
-parser.add_argument('--config_file', default=sys.argv[0] + '.conf', help='config file location')
-parser.add_argument('--log_file', default=sys.argv[0] + '.log', help='log file location')
-args = parser.parse_args()
-
 # init config dictionary
 conf = dict()
 
-# get config from json file
-conf_file = json.load(open(args.config_file))
+def parse_args():
+    '''Collect data and parse args'''
+    # get user name
+    user_name = os.environ['LOGNAME']
+    # set default filename
+    base_name = '{0}_{1}'.format(sys.argv[0], user_name)
 
-# add parsed from config_file to config dictionary
-for key in conf_file:
-    conf[key] = conf_file[key]
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file', default=base_name + '.conf', help='config file location')
+    parser.add_argument('--log_file', default=base_name + '.log', help='log file location')
+    parser.add_argument('--debug', action='store_const', const=True, help='enable debug mode - output to terminal')
+    args = parser.parse_args()
 
-# add parsed args to config dictionary
-for key in vars(args):
-    if vars(args)[key]:
-        conf[key] = vars(args)[key]
+    # set debug before any actions
+    conf['debug'] = vars(args)['debug']
 
-# main loop for interactive menu
-if __name__ == '__main__':
-    try:
-        # get and prepare command
-        command_tmp = os.environ['SSH_ORIGINAL_COMMAND']
-        command = shlex.split(command_tmp)
-        # write log
+    # get config from json file
+    conf_file = json.load(open(args.config_file))
+
+    # add parsed from config_file to config dictionary
+    for key in conf_file:
+        conf[key] = conf_file[key]
+
+    # add parsed args to config dictionary
+    for key in vars(args):
+        if vars(args)[key]:
+            conf[key] = vars(args)[key]
+
+def _write_log(message):
+    '''Write log to log file'''
+    if conf.get('debug'):
+        print(message)
+    else:
         with open(conf['log_file'], 'w') as log:
-            log.write("splited_command: {0}\noriginal_command: '{1}'\n".format(command, command_tmp))
-        # accept only allowed commannds
-        if command_tmp in conf['allowed_commands']:
-            proc0 = Popen(command, stdout=sys.stdout, stderr=sys.stderr)
-            proc = proc0.communicate()
-            proc0.wait()
-    except:
-        if conf.get('debug'):
-            trace = traceback.format_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
-            with open(conf['log_file'], 'a') as log:
-                log.write(''.join(trace))
+            log.write(message)
 
+def get_trace():
+    '''Get trace and write it to log'''
+    trace = traceback.format_exception(sys.exc_info()[0], sys.exc_info()[1], sys.exc_info()[2])
+    message = ''.join(trace)
+    _write_log(message)
+    sys.exit(1)
+
+def run_command():
+    '''Run command from ssh'''
+    # get and prepare command
+    command_tmp = os.environ['SSH_ORIGINAL_COMMAND']
+    command = shlex.split(command_tmp)
+
+    # write log
+    message = "===+===\nsplited_command: {0}\noriginal_command: '{1}'\n".format(command, command_tmp)
+    _write_log(message)
+    # accept only allowed commands
+    if command_tmp in conf['allowed_commands']:
+        proc0 = Popen(command, stdout=sys.stdout, stderr=sys.stderr)
+        proc = proc0.communicate()
+        proc0.wait()
+
+def run():
+    '''Run whole script'''
+    parse_args()
+    run_command()
+
+if __name__ == '__main__':
+   #run()
+    try:
+        run()
+    except:
+        get_trace()
 
